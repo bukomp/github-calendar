a0888879e21fd3104032a9dd12067ef351cd4f56
Edvard <edvard@shalaev.com>
2023-10-18 21:51:59 +0300

    initialized calendar and mock data for it, for demonstration purposes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { userCommits_mock } from './mock-data/UserCommits';
+import { GitCommit } from './components/heatmap-calendar/interfaces/git.interface';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'GitCalendarApp';
+  mock_data: GitCommit[] = userCommits_mock.map((c) => {
+    c.date = new Date(c.date);
+    return c;
+  });
 }

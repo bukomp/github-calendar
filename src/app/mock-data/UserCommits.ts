6597f3cf85f7be39d5864a611f789be36c3bcc62
Edvard <edvard@shalaev.com>
2023-10-18 21:51:13 +0300

    added mock data (based on fetched github commit history data)

---
+++ src/app/mock-data/UserCommits.ts
@@ -0,0 +1,541 @@
+import { GitCommit } from '../components/heatmap-calendar/interfaces/git.interface';
+
+export const userCommits_mock: any[] = [
+  {
+    sha: '34fdbccfcc7698de8ed31a68d1e32b77cc017fba',
+    author: {
+      email: 'edvard@shalaev.com',
+      name: 'Edvard',
+    },
+    message: 'fix: fixed something in scripts',
+    distinct: true,
+    url: 'https://api.github.com/repos/bukomp/mAIcroservicer/commits/34fdbccfcc7698de8ed31a68d1e32b77cc017fba',
+    date: '2023-10-18T18:43:00.888Z',
+  },
+  {
+    sha: '34fdbccfcc7698de8ed31a68d1e32b77cc017fba',
+    author: {
+      email: 'edvard@shalaev.com',
+      name: 'Edvard',
+    },
+    message: 'fix: fixed something in scripts',
+    distinct: true,
+    url: 'https://api.github.com/repos/bukomp/mAIcroservicer/commits/34fdbccfcc7698de8ed31a68d1e32b77cc017fba',
+    date: '2023-10-16T17:06:39.000Z',
+  },
+  {
+    sha: 'decc6a4d753f74a00c1f70a90f096724ec405dcd',
+    author: {
+      email: 'edvard@shalaev.com',
+      name: 'Edvard',
+    },
+    message: 'feat: added jsfuck microservice endpoint test',
+    distinct: true,
+    url: 'https://api.github.com/repos/bukomp/mAIcroservicer/commits/decc6a4d753f74a00c1f70a90f096724ec405dcd',
+    date: '2023-10-16T17:06:12.000Z',
+  },
+  {
+    sha: '4a93e2a490afa486631756aa1a22c74337e486cc',
+    author: {
+      email: 'edvard@shalaev.com',
+      name: 'Edvard',
+    },
+    message:
+      'feat: added posibility to give input prompt to the program by calling the app with -p flag specified',
+    distinct: true,
+    url: 'https://api.github.com/repos/bukomp/mAIcroservicer/commits/4a93e2a490afa486631756aa1a22c74337e486cc',
+    date: '2023-10-16T17:05:52.000Z',
+  },
+  {
+    sha: '35df343f9d9f593fc33aaa4e1579d9ca44e5a05b',
+    author: {
+      email: 'edvard@shalaev.com',
+      name: 'Edvard',
+    },
+    message:
+      'feat: refactored code to create separate files for input/output prompts before creation is complete',
+    distinct: true,
+    url: 'https://api.github.com/repos/bukomp/mAIcroservicer/commits/35df343f9d9f593fc33aaa4e1579d9ca44e5a05b',
+    date: '2023-10-16T17:05:16.000Z',
+  },
+  {
+    sha: 'f0963a36da9aebedba51be008247df3f5e24fef6',
+    author: {
+      email: 'edvard@shalaev.com',
+      name: 'Edvard',
+    },
+    message:
+      'feat: added multiple new versions of system prompts, currently the best working one is v4',
+    distinct: true,
+    url: 'https://api.github.com/repos/bukomp/mAIcroservicer/commits/f0963a36da9aebedba51be008247df3f5e24fef6',
+    date: '2023-10-16T17:03:52.000Z',
+  },
+  {
+    sha: '3ffd15b513ff2cb84ba5e6d89ea1e944ec592beb',
+    author: {
+      email: 'edvard@shalaev.com',
+      name: 'Edvard',
+    },
+    message: 'fix: renamed prompt library files for easier imports',
+    distinct: true,
+    url: 'https://api.github.com/repos/bukomp/mAIcroservicer/commits/3ffd15b513ff2cb84ba5e6d89ea1e944ec592beb',
+    date: '2023-10-16T14:35:51.000Z',
+  },
+  {
+    sha: 'd3869bf49671a46c8b50c4c3594d09e0d92a5c18',
+    author: {
+      email: 'edvard@shalaev.com',
+      name: 'Edvard',
+    },
+    message: 'Added GNU GPLv3 licence',
+    distinct: true,
+    url: 'https://api.github.com/repos/bukomp/mAIcroservicer/commits/d3869bf49671a46c8b50c4c3594d09e0d92a5c18',
+    date: '2023-10-16T14:19:57.000Z',
+  },
+  {
+    sha: '5c96710c4a66947d2a7f5847bf854a3e52f25fd7',
+    author: {
+      email: 'edvard@shalaev.com',
+      name: 'Edvard',
+    },
+    message:
+      "feat: Added 'hyper torture' mode\n\nSquashed commit of the following:\n    fix: fixed storage\n    added hyper torture condition for the fail modal window\n    Added separate streak funtionality for hyper torture mode\n    added image and popup modal with haha funny image\n    Merge branch 'main' into hyper_torture\n    changed index popup for hyperTorture mode\n    added hyperTorture streak saving/updating\n    hyper torture mode redirect logic done\n    Added hypertorture mode messages and UI elements\n\nCloses #89",
+    distinct: true,
+    url: 'https://api.github.com/repos/bukomp/haha-funny-leetcode-extension/commits/5c96710c4a66947d2a7f5847bf854a3e52f25fd7',
+    date: '2023-10-15T02:17:11.000Z',
+  },
+  {
+    sha: '646a9aee4144d0014b34a3dcffc0f28a29d3c5b7',
+    author: {
+      email: 'edvard@shalaev.com',
+      name: 'Edvard',
+    },
+    message: 'fix: fixed storage',
+    distinct: true,
+    url: 'https://api.github.com/repos/bukomp/haha-funny-leetcode-extension/commits/646a9aee4144d0014b34a3dcffc0f28a29d3c5b7',
+    date: '2023-10-15T01:28:22.000Z',
+  },
+  {
+    sha: 'd0d5c9079474a0fed6bff5e82ddcc22e5997ed4f',
+    author: {
+      email: 'edvard@shalaev.com',
+      name: 'Edvard',
+    },
+    message: 'added hyper torture condition for the fail modal window',
+    distinct: true,
+    url: 'https://api.github.com/repos/bukomp/haha-funny-leetcode-extension/commits/d0d5c9079474a0fed6bff5e82ddcc22e5997ed4f',
+    date: '2023-10-15T01:10:34.000Z',
+  },
+  {
+    sha: 'e5660fc7bb68fcc649f5e9b1c3556624a9b62e03',
+    author: {
+      email: 'edvard@shalaev.com',
+      name: 'Edvard',
+    },
+    message: 'Added separate streak funtionality for hyper torture mode',
+    distinct: true,
+    url: 'https://api.github.com/repos/bukomp/haha-funny-leetcode-extension/commits/e5660fc7bb68fcc649f5e9b1c3556624a9b62e03',
+    date: '2023-10-15T00:56:35.000Z',
+  },
+  {
+    sha: 'af70b7e01bc5b5fe07010b2d66390e8a790e1303',
+    author: {
+      email: 'edvard@shalaev.com',
+      name: 'Edvard',
+    },
+    message: 'added image and popup modal with haha funny image',
+    distinct: true,
+    url: 'https://api.github.com/repos/bukomp/haha-funny-leetcode-extension/commits/af70b7e01bc5b5fe07010b2d66390e8a790e1303',
+    date: '2023-10-15T00:53:42.000Z',
+  },
+  {
+    sha: '27ef94a451c3c80784d2f41e9872a2850e45052d',
+    author: {
+      email: '44459524+bukomp@users.noreply.github.com',
+      name: 'Edvard Shalaev',
+    },
+    message: 'I GOT NEW FOLLOWER XD',
+    distinct: true,
+    url: 'https://api.github.com/repos/bukomp/bukomp/commits/27ef94a451c3c80784d2f41e9872a2850e45052d',
+    date: '2023-10-14T17:25:41.000Z',
+  },
+  {
+    sha: '1a354e8e853bc5715c179f350f028c10f35917c0',
+    author: {
+      email: '44459524+bukomp@users.noreply.github.com',
+      name: 'Edvard Shalaev',
+    },
+    message: 'made readme bit more simpler',
+    distinct: true,
+    url: 'https://api.github.com/repos/bukomp/bukomp/commits/1a354e8e853bc5715c179f350f028c10f35917c0',
+    date: '2023-10-14T13:44:00.000Z',
+  },
+  {
+    sha: 'a14e831868c13f780cb9ff36ee49611a766caa30',
+    author: {
+      email: '44459524+bukomp@users.noreply.github.com',
+      name: 'Edvard Shalaev',
+    },
+    message: 'removed dot',
+    distinct: true,
+    url: 'https://api.github.com/repos/bukomp/bukomp/commits/a14e831868c13f780cb9ff36ee49611a766caa30',
+    date: '2023-10-13T00:06:16.000Z',
+  },
+  {
+    sha: 'f2122e581764002c45a79aad0cb2b23834f85c40',
+    author: {
+      email: '44459524+bukomp@users.noreply.github.com',
+      name: 'Edvard Shalaev',
+    },
+    message: 'minor clarifications',
+    distinct: true,
+    url: 'https://api.github.com/repos/bukomp/bukomp/commits/f2122e581764002c45a79aad0cb2b23834f85c40',
+    date: '2023-10-13T00:04:34.000Z',
+  },
+  {
+    sha: 'c7941f85e81e267db03854b047cde60e705b7ece',
+    author: {
+      email: '44459524+bukomp@users.noreply.github.com',
+      name: 'Edvard Shalaev',
+    },
+    message: 'minor text changes',
+    distinct: true,
+    url: 'https://api.github.com/repos/bukomp/bukomp/commits/c7941f85e81e267db03854b047cde60e705b7ece',
+    date: '2023-10-13T00:00:35.000Z',
+  },
+  {
+    sha: '47cac4e53a3864452aa25ea8173a33b8c139aaf8',
+    author: {
+      email: 'edvard@shalaev.com',
+      name: 'Edvard',
+    },
+    message: 'fixed minor styling issue',
+    distinct: true,
+    url: 'https://api.github.com/repos/bukomp/bukomp/commits/47cac4e53a3864452aa25ea8173a33b8c139aaf8',
+    date: '2023-10-12T23:50:32.000Z',
+  },
+  {
+    sha: 'b2eb60d1285f605e91c53e0ce6be5b0d345758c5',
+    author: {
+      email: 'edvard@shalaev.com',
+      name: 'Edvard',
+    },
+    message: "updated readme to follow github's rules",
+    distinct: true,
+    url: 'https://api.github.com/repos/bukomp/bukomp/commits/b2eb60d1285f605e91c53e0ce6be5b0d345758c5',
+    date: '2023-10-12T23:49:00.000Z',
+  },
+  {
+    sha: 'c8241455b260d44d006ede785c031a76a4451d53',
+    author: {
+      email: 'edvard@shalaev.com',
+      name: 'Edvard',
+    },
+    message: 'first try',
+    distinct: true,
+    url: 'https://api.github.com/repos/bukomp/bukomp/commits/c8241455b260d44d006ede785c031a76a4451d53',
+    date: '2023-10-12T22:29:15.000Z',
+  },
+  {
+    sha: '6f1b3306ab944e9a6c33094bc2ca9080505b0949',
+    author: {
+      email: '143575542+The-CodingSloth@users.noreply.github.com',
+      name: 'The Coding Sloth',
+    },
+    message:
+      'Merge pull request #85 from Mgel/feature/refactor-storage\n\nRefactored storage',
+    distinct: true,
+    url: 'https://api.github.com/repos/bukomp/haha-funny-leetcode-extension/commits/6f1b3306ab944e9a6c33094bc2ca9080505b0949',
+    date: '2023-10-02T20:17:51.000Z',
+  },
+  {
+    sha: 'a7ee96340680dff1e5a028d62c88be4d72bcadab',
+    author: {
+      email: '143575542+The-CodingSloth@users.noreply.github.com',
+      name: 'The Coding Sloth',
+    },
+    message:
+      'Merge pull request #91 from Tayomide/url-parsing\n\nRefine title to url parsing to resolve #88',
+    distinct: true,
+    url: 'https://api.github.com/repos/bukomp/haha-funny-leetcode-extension/commits/a7ee96340680dff1e5a028d62c88be4d72bcadab',
+    date: '2023-09-29T22:25:18.000Z',
+  },
+  {
+    sha: '69902f2325a990e9b10b2410676ec17d501ea121',
+    author: {
+      email: '70061548+Tayomide@users.noreply.github.com',
+      name: 'Tayomide',
+    },
+    message: 'Refine title to url parsing to resolve #88',
+    distinct: true,
+    url: 'https://api.github.com/repos/bukomp/haha-funny-leetcode-extension/commits/69902f2325a990e9b10b2410676ec17d501ea121',
+    date: '2023-09-29T18:20:08.000Z',
+  },
+  {
+    sha: '248ed082c06c031624a30784b3c6164608031cd0',
+    author: {
+      email: 'mikegelarsen@gmail.com',
+      name: 'Mike Larsen',
+    },
+    message: 'Use Promise.all and try catch block',
+    distinct: true,
+    url: 'https://api.github.com/repos/bukomp/haha-funny-leetcode-extension/commits/248ed082c06c031624a30784b3c6164608031cd0',
+    date: '2023-09-27T22:37:13.000Z',
+  },
+  {
+    sha: 'dc92a991960971e7fc12ffc427704750127d0e81',
+    author: {
+      email: 'mikegelarsen@gmail.com',
+      name: 'Mike Larsen',
+    },
+    message: 'Refactored storage into its own file with some helper methods',
+    distinct: true,
+    url: 'https://api.github.com/repos/bukomp/haha-funny-leetcode-extension/commits/dc92a991960971e7fc12ffc427704750127d0e81',
+    date: '2023-09-25T21:35:50.000Z',
+  },
+  {
+    sha: 'f71583c2598818aad88903dfc33e549063ce3104',
+    author: {
+      email: 'edvard@shalaev.com',
+      name: 'Edvard',
+    },
+    message:
+      'feat: midi/music player logic half done. Now play/pause features work',
+    distinct: true,
+    url: 'https://api.github.com/repos/bukomp/my-page/commits/f71583c2598818aad88903dfc33e549063ce3104',
+    date: '2023-09-25T19:11:53.000Z',
+  },
+  {
+    sha: 'ac3cfc4ef37be9470f2377f1bcaa023c6a78ee56',
+    author: {
+      email: 'edvard@shalaev.com',
+      name: 'Edvard',
+    },
+    message: 'got midi player working',
+    distinct: true,
+    url: 'https://api.github.com/repos/bukomp/my-page/commits/ac3cfc4ef37be9470f2377f1bcaa023c6a78ee56',
+    date: '2023-09-25T16:15:54.000Z',
+  },
+  {
+    sha: 'f27e936824568ebcd97d4c8b2c40916de5071e3f',
+    author: {
+      email: 'edvard@shalaev.com',
+      name: 'Edvard',
+    },
+    message: 'added another endpoint to rest testing file',
+    distinct: false,
+    url: 'https://api.github.com/repos/bukomp/mAIcroservicer/commits/f27e936824568ebcd97d4c8b2c40916de5071e3f',
+    date: '2023-09-08T20:32:48.000Z',
+  },
+  {
+    sha: '103d1eebc9b585d0d84e3fcb3483009b1c44176e',
+    author: {
+      email: 'edvard@shalaev.com',
+      name: 'Edvard',
+    },
+    message: 'feat: upgraded prompts & added temperature setting',
+    distinct: false,
+    url: 'https://api.github.com/repos/bukomp/mAIcroservicer/commits/103d1eebc9b585d0d84e3fcb3483009b1c44176e',
+    date: '2023-09-06T21:08:33.000Z',
+  },
+  {
+    sha: 'ad89fb5360e2b97fb2de53db07937cb54758152c',
+    author: {
+      email: 'edvard@shalaev.com',
+      name: 'Edvard',
+    },
+    message: 'fix: removed temp folder',
+    distinct: false,
+    url: 'https://api.github.com/repos/bukomp/mAIcroservicer/commits/ad89fb5360e2b97fb2de53db07937cb54758152c',
+    date: '2023-09-06T18:58:39.000Z',
+  },
+  {
+    sha: '03ea11d61e4035523446be68f5ff4bc1aaea6c2a',
+    author: {
+      email: 'edvard@shalaev.com',
+      name: 'Edvard',
+    },
+    message: 'fix: changed gitignore',
+    distinct: false,
+    url: 'https://api.github.com/repos/bukomp/mAIcroservicer/commits/03ea11d61e4035523446be68f5ff4bc1aaea6c2a',
+    date: '2023-09-06T18:57:12.000Z',
+  },
+  {
+    sha: 'a8b44ca4750ce0b7a29719e05aab13391f91d010',
+    author: {
+      email: 'edvard@shalaev.com',
+      name: 'Edvard',
+    },
+    message: 'feat: mAicroservicer works, now to the testing phase',
+    distinct: false,
+    url: 'https://api.github.com/repos/bukomp/mAIcroservicer/commits/a8b44ca4750ce0b7a29719e05aab13391f91d010',
+    date: '2023-09-04T19:42:56.000Z',
+  },
+  {
+    sha: '43c6fe1870176ac6966b18fbcb1732028a8bea2d',
+    author: {
+      email: 'edvard@shalaev.com',
+      name: 'Edvard',
+    },
+    message: 'started working on integrations',
+    distinct: false,
+    url: 'https://api.github.com/repos/bukomp/mAIcroservicer/commits/43c6fe1870176ac6966b18fbcb1732028a8bea2d',
+    date: '2023-09-03T21:18:21.000Z',
+  },
+  {
+    sha: '87454b29332e539183447411adc8dea938cddce8',
+    author: {
+      email: 'edvard@shalaev.com',
+      name: 'Edvard',
+    },
+    message: 'feat: microservice generated by GPT worked',
+    distinct: false,
+    url: 'https://api.github.com/repos/bukomp/mAIcroservicer/commits/87454b29332e539183447411adc8dea938cddce8',
+    date: '2023-09-03T12:15:34.000Z',
+  },
+  {
+    sha: 'ee9a213c2724a95ceb1287b0f234356ba2708552',
+    author: {
+      email: 'edvard@shalaev.com',
+      name: 'Edvard',
+    },
+    message: 'feat: created python microservice version with tests and all',
+    distinct: false,
+    url: 'https://api.github.com/repos/bukomp/mAIcroservicer/commits/ee9a213c2724a95ceb1287b0f234356ba2708552',
+    date: '2023-09-02T22:05:32.000Z',
+  },
+  {
+    sha: '88cb082cc9ce6f7c1f406b1753d27bf3ba4db225',
+    author: {
+      email: 'edvard@shalaev.com',
+      name: 'Edvard',
+    },
+    message: 'feat: file structure extractor script added',
+    distinct: false,
+    url: 'https://api.github.com/repos/bukomp/mAIcroservicer/commits/88cb082cc9ce6f7c1f406b1753d27bf3ba4db225',
+    date: '2023-09-02T19:44:59.000Z',
+  },
+  {
+    sha: '30ef970a4f12c4e35fa054a00820e894385dbdc0',
+    author: {
+      email: 'edvard@shalaev.com',
+      name: 'Edvard',
+    },
+    message: 'feat: added tests & buid, start scripts',
+    distinct: false,
+    url: 'https://api.github.com/repos/bukomp/mAIcroservicer/commits/30ef970a4f12c4e35fa054a00820e894385dbdc0',
+    date: '2023-09-02T13:56:15.000Z',
+  },
+  {
+    sha: 'b12444c135c6c2dee8cea8a2d957ba940f1cba8a',
+    author: {
+      email: 'edvard@shalaev.com',
+      name: 'Edvard',
+    },
+    message:
+      "Merge branch 'development' of github.com:bukomp/mAIcroserviser into development",
+    distinct: false,
+    url: 'https://api.github.com/repos/bukomp/mAIcroservicer/commits/b12444c135c6c2dee8cea8a2d957ba940f1cba8a',
+    date: '2023-08-29T18:24:47.000Z',
+  },
+  {
+    sha: '2020b100f8347e8bef6a779ac448b451582b1c4e',
+    author: {
+      email: 'edvard@shalaev.com',
+      name: 'Edvard',
+    },
+    message: 'fix: fixed app',
+    distinct: false,
+    url: 'https://api.github.com/repos/bukomp/mAIcroservicer/commits/2020b100f8347e8bef6a779ac448b451582b1c4e',
+    date: '2023-08-29T16:50:12.000Z',
+  },
+  {
+    sha: '47fae277b69387474eec954c8a449b5707bdd957',
+    author: {
+      email: 'edvard@shalaev.com',
+      name: 'Edvard',
+    },
+    message: 'fix: fixed app',
+    distinct: false,
+    url: 'https://api.github.com/repos/bukomp/mAIcroservicer/commits/47fae277b69387474eec954c8a449b5707bdd957',
+    date: '2023-08-29T16:50:12.000Z',
+  },
+  {
+    sha: 'f8a1214385f0b6cd1acf73f31e428f91ddde8350',
+    author: {
+      email: 'edvard@shalaev.com',
+      name: 'Edvard',
+    },
+    message: 'fix: updated controller to use async exec & created REST test',
+    distinct: false,
+    url: 'https://api.github.com/repos/bukomp/mAIcroservicer/commits/f8a1214385f0b6cd1acf73f31e428f91ddde8350',
+    date: '2023-08-29T16:49:55.000Z',
+  },
+  {
+    sha: '64ca6096848ddb0aecffb43039ff02777d12e074',
+    author: {
+      email: 'edvard@shalaev.com',
+      name: 'Edvard',
+    },
+    message: 'feat: created functionality for cli app',
+    distinct: false,
+    url: 'https://api.github.com/repos/bukomp/mAIcroservicer/commits/64ca6096848ddb0aecffb43039ff02777d12e074',
+    date: '2023-08-29T16:34:15.000Z',
+  },
+  {
+    sha: 'f5681043615050ac65eed3446a6ce1143836ce68',
+    author: {
+      email: 'edvard@shalaev.com',
+      name: 'Edvard',
+    },
+    message: 'feat: redid controller for cli app calls',
+    distinct: false,
+    url: 'https://api.github.com/repos/bukomp/mAIcroservicer/commits/f5681043615050ac65eed3446a6ce1143836ce68',
+    date: '2023-08-29T16:31:44.000Z',
+  },
+  {
+    sha: '32391fb17c6bdfec785e4064348b16635cb3fcb6',
+    author: {
+      email: 'edvard@shalaev.com',
+      name: 'Edvard',
+    },
+    message: 'fix: fixed naming',
+    distinct: false,
+    url: 'https://api.github.com/repos/bukomp/mAIcroservicer/commits/32391fb17c6bdfec785e4064348b16635cb3fcb6',
+    date: '2023-08-29T16:23:35.000Z',
+  },
+  {
+    sha: '23e13aa40e4ebdff7b242453dda1a1d736f2a2c3',
+    author: {
+      email: 'edvard@shalaev.com',
+      name: 'Edvard',
+    },
+    message:
+      'feat: new skeleton built, now implementing functionality for proof of concept',
+    distinct: false,
+    url: 'https://api.github.com/repos/bukomp/mAIcroservicer/commits/23e13aa40e4ebdff7b242453dda1a1d736f2a2c3',
+    date: '2023-08-29T16:21:58.000Z',
+  },
+  {
+    sha: 'f872806a6a32e2f9efb592f6dd1b92d104584768',
+    author: {
+      email: 'edvard@shalaev.com',
+      name: 'Edvard',
+    },
+    message: 'feat: added template for the documentation',
+    distinct: false,
+    url: 'https://api.github.com/repos/bukomp/mAIcroservicer/commits/f872806a6a32e2f9efb592f6dd1b92d104584768',
+    date: '2023-08-26T19:55:08.000Z',
+  },
+  {
+    sha: '65d1695689ac76ab88b14f5d3e5a156c141a340d',
+    author: {
+      email: 'edvard@shalaev.com',
+      name: 'Edvard',
+    },
+    message: 'feat: fixes to the apps, now they run',
+    distinct: false,
+    url: 'https://api.github.com/repos/bukomp/mAIcroservicer/commits/65d1695689ac76ab88b14f5d3e5a156c141a340d',
+    date: '2023-08-26T19:49:20.000Z',
+  },
+];
